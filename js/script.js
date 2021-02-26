85116a94580f9587c0357f93cba9c1b50da35e1e
yalloo <so-_-what@naver.com>
2021-02-26 16:16:51 +0900

    mo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".menu li:nth-child(1)").click(function(){
-        $("html, body").animate({scrollTop:$("#cont_01 article:nth-child(1)").offset().top},1000);
+        $("html, body").animate({scrollTop:$("#cont_01 div:nth-child(1)").offset().top},1000);
         return false;
     })
     $(".menu li:nth-child(2)").click(function(){
